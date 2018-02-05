a06b8add8662ee8f3b38c15c07914c13f99df16d
androidbyvinti <32050504+androidbyvinti@users.noreply.github.com>
2018-02-05 13:34:44 +0530

    Add files via upload

---
+++ 2 Feb- Class Data and Exercise/firstapp/Hello.kt
@@ -0,0 +1,7 @@
+package com.bmpl.android.firstapp
+
+fun main(args : Array<String>){
+
+    println("hello kotlin")
+
+}
---
+++ 2 Feb- Class Data and Exercise/firstapp/Hello1.kt
@@ -0,0 +1,15 @@
+package com.bmpl.android.firstapp
+
+
+    fun main(args: Array<String>) {
+
+        var value1 = Int
+
+        var a = 89
+        var b = 43
+        var c = a
+
+        println(a == c)
+
+        //Reference type -->
+    }
